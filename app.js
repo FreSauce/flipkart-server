e1dca1d649ad032d69d23dabf4ae649d5b182f4f
khanmn <khanmohdfaisal75@gmail.com>
2022-07-18 10:38:13 +0530

    auth done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const AppError = require("./AppError");
+const userRouter = require("./routes/userRoutes");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+app.use(cors());
+// app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.body);
+  next();
+});
+
+app.use("/api/users", userRouter);
+
+app.use("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  console.log(err);
+  res.status(statusCode).json({
+    status: "error",
+    message: statusCode === 500 ? "Something went wrong" : message,
+  });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
+dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 3000;
+const app = require("./app");
+const DB = process.env.DATABASE_STRING;
+
+console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((_) => {
+    console.log("database successfully connected");
+  });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
