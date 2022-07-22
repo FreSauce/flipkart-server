c212f5175d9907ff8aeffb8304cc3e48cac92317
khanmn <khanmohdfaisal75@gmail.com>
2022-07-22 13:03:33 +0530

    code changed

--- controller/userController.js
+++ controller/userController.js
@@ -25,14 +25,15 @@ exports.storeCharacterData = async (req, res, next) => {
 exports.getCharacterData = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id);
-    const parsedCharacterData = JSON.parse(user.character_data);
-    // console.log(parsedCharacterData["settingsName"]);
-    // console.log(2);
     let characterType;
-    if (parsedCharacterData["settingsName"] === "FemaleSettings")
-      characterType = "Female";
-    if (parsedCharacterData["settingsName"] === "MaleSettings")
-      characterType = "Male";
+    console.log(user);
+    if (user.character_data !== "") {
+      const parsedCharacterData = JSON.parse(user.character_data);
+      if (parsedCharacterData["settingsName"] === "FemaleSettings")
+        characterType = "Female";
+      if (parsedCharacterData["settingsName"] === "MaleSettings")
+        characterType = "Male";
+    }
     res.status(200).json({
       status: "success",
       characterData: user.character_data,
@@ -47,16 +48,17 @@ exports.getCharacterData = async (req, res, next) => {
 
 exports.getUserData = async (req, res, next) => {
   try {
-    console.log(req.params.id);
     const user = await User.findById(req.params.id);
-    console.log(user + "LINE 52");
-    const parsedCharacterData = JSON.parse(user.character_data);
-
     let characterType;
-    if (parsedCharacterData["settingsName"] === "FemaleSettings")
-      characterType = "Female";
-    if (parsedCharacterData["settingsName"] === "MaleSettings")
-      characterType = "Male";
+    console.log(user);
+    if (user.character_data !== "") {
+      const parsedCharacterData = JSON.parse(user.character_data);
+      if (parsedCharacterData["settingsName"] === "FemaleSettings")
+        characterType = "Female";
+      if (parsedCharacterData["settingsName"] === "MaleSettings")
+        characterType = "Male";
+    }
+
     res.status(200).json({
       status: "success",
       characterData: user.character_data,
